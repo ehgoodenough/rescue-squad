b3da1eb46a068ae83c3fe20865f6fe93cc3a72ad
Andrew McPherson <andrewmcp333@gmail.com>
2016-06-04 21:44:19 -0700

    Fixed death-by-gravity from speed to distance.

--- source/scripts/object/Player.js
+++ source/scripts/object/Player.js
@@ -3,6 +3,8 @@ const VERTICALITY = 10
 const GRAVITY = 0.9
 const TOO_MUCH_GRAVITY = 10
 
+import {UNIT} from "../utility/Constants.js"
+
 export default class Player {
     constructor(player) {
         this.position = player.position
@@ -16,6 +18,7 @@ export default class Player {
 
         this.velocity = {x: 0, y: 0}
         this.acceleration = {x: 5, y: 8}
+        this.jumpdist = 0
 
         this.equipment = {
             "parachutes": 2,
@@ -26,45 +29,38 @@ export default class Player {
         this.stack = 99
     }
     update(delta) {
-        if(this.position.x - this.game.stage.levels[this.level].speed > 0) {
-            this.position.x -= this.game.stage.levels[this.level].speed
-        } else if(this.game.stage.levels[this.level].y(this.position.x) - this.position.y < -VERTICALITY) {
-            this.game.stage.mode = "game over"
+        if(this.position.x - this.stage.levels[this.level].speed > 0) {
+            this.position.x -= this.stage.levels[this.level].speed
+        } else if(this.stage.levels[this.level].y(this.position.x) - this.position.y < -VERTICALITY) {
+            this.stage.mode = "game over"
         }
 
         // vertical acceleration from inputs
-        if(this.mode == "dropping"
-        || this.mode == "falling"
-        || this.mode == "jumping") {
-            if(this.equipment.parachutes > 0) {
-                if(this.inputs["down"].isJustDown(delta)) {
-                    this.equipment.parachutes -= 1
-                    this.mode = "parachuting"
-                }
-            }
+        if(this.equipment.parachutes > 0
+        && this.inputs["down"].isJustDown(delta)
+        && ["dropping", "falling", "jumping"].indexOf(this.mode) != -1) {
+            this.equipment.parachutes -= 1
+            this.mode = "parachuting"
         }
-        if(this.mode == "jumping"
-        || this.mode == "falling"
-        || this.mode == "dropping") {
-            if(this.equipment.ropes > 0) {
-                if(this.level > 0) {
-                    if(this.inputs["up"].isJustDown(delta)) {
-                        this.equipment.ropes -= 1
-                        this.mode = "hiking"
-                    }
-                }
-            }
+        if(this.level > 0
+        && this.equipment.ropes > 0
+        && this.inputs["up"].isJustDown(delta)
+        && ["dropping", "falling", "jumping"].indexOf(this.mode) != -1) {
+            this.equipment.ropes -= 1
+            this.mode = "hiking"
         }
         if(this.mode == "on ground" && this.inputs["up"].isDown()
         || this.mode == "on ledge" && this.inputs["up"].isJustDown(delta)
         || this.mode == "falling" && this.velocity.y < 5 && this.inputs["up"].isJustDown(delta)) {
             this.velocity.y = -1 * this.acceleration.y
             this.mode = "jumping"
+            this.jumpdist = this.position.y
         }
         if(this.mode == "on ledge" && this.inputs["down"].isJustDown(delta)
         || this.mode == "on ground" && this.inputs["down"].isJustDown(delta)) {
-            if(this.level < this.game.stage.levels.length - 1) {
+            if(this.level < this.stage.levels.length - 1) {
                 this.mode = "dropping"
+                this.jumpdist = this.position.y
                 this.level += 1
             }
         }
@@ -92,7 +88,7 @@ export default class Player {
         }
 
         // query level
-        var level = this.game.stage.levels[this.level]
+        var level = this.stage.levels[this.level]
 
         // collision with camera
         if(this.position.x + this.velocity.x < 0
@@ -107,32 +103,30 @@ export default class Player {
         if(level.y(this.position.x + this.velocity.x) - this.position.y > +VERTICALITY) {
             if(this.mode == "on ground") {
                 this.mode = "falling"
+                this.jumpdist = this.position.y
             }
         }
 
         // vertical collision with level
         if(this.mode == "jumping" || this.mode == "falling" || this.mode == "dropping" || this.mode == "parachuting") {
-            if(this.level > 0 && this.position.y < this.game.stage.levels[this.level - 1].y(this.position.x + this.velocity.x)) {
+            if(this.level > 0 && this.position.y < this.stage.levels[this.level - 1].y(this.position.x + this.velocity.x)) {
                 this.level -= 1
             }
         }
-        // if(this.mode == "hiking") {
-        //     if(this.level > 0 && this.position.y < this.game.stage.levels[this.level - 1].y(this.position.x + this.velocity.x)) {
-        //         this.level -= 1
-        //         this.mode = "on ledge"
-        //     }
-        // }
         if(((this.mode == "jumping" && this.velocity.y > 0) || this.mode == "hiking")
-        && this.level > 0 && this.position.y - this.height < this.game.stage.levels[this.level - 1].y(this.position.x + this.velocity.x)
-        && level.y(this.position.x + this.velocity.x) - this.game.stage.levels[this.level - 1].y(this.position.x + this.velocity.x) > this.height) {
+        && this.level > 0 && this.position.y - this.height < this.stage.levels[this.level - 1].y(this.position.x + this.velocity.x)
+        && level.y(this.position.x + this.velocity.x) - this.stage.levels[this.level - 1].y(this.position.x + this.velocity.x) > this.height) {
             this.mode = "on ledge"
             this.level -= 1
         }
         if(this.velocity.y > 0
         && this.position.y + this.velocity.y - (this.mode == "on ledge" ? this.height : 0) > level.y(this.position.x + this.velocity.x)) {
             this.position.y = level.y(this.position.x + this.velocity.x) + (this.mode == "on ledge" ? this.height : 0)
-            if(this.velocity.y > TOO_MUCH_GRAVITY) {
-                this.game.stage.mode = "game over"
+            console.log(this.position.y, this.jumpdist, this.position.y - this.jumpdist)
+            if(["jumping", "falling", "dropping"].indexOf(this.mode) != -1) {
+                if(this.position.y - this.jumpdist >= UNIT * 2) {
+                    this.stage.mode = "game over"
+                }
             }
             this.velocity.y = 0
             if(this.mode != "on ledge") {
